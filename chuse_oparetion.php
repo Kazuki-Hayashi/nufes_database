918d35d948c88d3f5a1d9b662837e753f19094d2
Kazuki-Hayashi <kazuki.hwahee.sep.14@gmail.com>
2017-06-18 21:44:37 +0900

    Add files via upload

---
+++ chuse_oparetion.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="ja">
+	<head>
+		<meta charset="UTF-8">
+		<title>Chose Operation</title>
+		<meta name="author" content="58th WEB CHIEF">
+		<meta name="viewport" content="width=device-width,initial-scale=1.0">
+		<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/3.18.1/build/cssreset/cssreset-min.css">
+		<link rel="stylesheet" href="./css/_style.css">
+	</head>
+	<body>
+		<div class="wrap">
+			<div class="sub">
+				<h3>表示するデータを選んでください。</h3>
+			</div>
+		</div>
+	</body>
+</html>
